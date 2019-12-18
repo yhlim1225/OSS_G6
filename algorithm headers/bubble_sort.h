eff47c20ee95fa2deade8cba16f697cb44f473d6
YOUNGHOON LIM <yhlim1225@hotmail.com>
2019-12-18 17:27:28 +0900

    Add bubble_sort.h

---
+++ algorithm headers/bubble_sort.h
@@ -0,0 +1,25 @@
+ifndef _BUBBLE_SORT_H
+define _BUBBLE_SORT_H
+
+#include <assert.h>
+
+template<typename T>
+static void BubbleSort(T item_list[], int start, int end)
+{
+	int i;
+	bool swap = true;
+	assert(start < end);
+	while(swap)
+	{
+		swap = false;
+		for(i = start+1; i <= end; i++)
+		{
+			if(item_list[i-1] > item_list[i])
+			{
+				swap(item_list[i-1], item_list[i]);
+				swap=true;
+			}
+		}
+	}
+}
+#endif
