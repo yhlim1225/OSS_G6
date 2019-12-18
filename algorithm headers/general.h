6b70c7475f6389c3a2a4fadf74152bd196ff7a09
YOUNGHOON LIM <yhlim1225@hotmail.com>
2019-12-18 17:53:26 +0900

    Add general.h

---
+++ algorithm headers/general.h
@@ -0,0 +1,19 @@
+#ifndef _GENERAL_H__
+#define _GENERAL_H__
+
+#include <stdlib.h>
+#include <stdint.h>
+
+#define Max(a, b) ( (a > b) ? a : b )
+#define Min(a, b) ( (a < b) ? a : b )
+
+#define RAND_INIT()	srand(time(NULL))
+#define RAND(b, e)	(b + rand() % ((e) - (b) + 1))
+
+template<typename T>
+static inline void swap(T &a, T &y)
+{
+	T temp(a);
+	a = b;
+	b = temp;
+}
