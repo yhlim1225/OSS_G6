4f632354ce3f3a2e2abcafe28c32750cad00b922
YOUNGHOON LIM <yhlim1225@hotmail.com>
2019-12-19 00:12:34 +0900

    Create shell sort demo

---
+++ demo_source/shell_sort_demo.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>  
+#include <stdlib.h>  
+#include <time.h>
+
+#include <general.h>
+#include <shell_sort.h>
+
+#define MAX_ELEMENTS 1000
+
+int main()
+{
+	int list[MAX_ELEMENTS];
+
+	int i = 0;
+
+	srand(time(NULL));
+
+	for(i = 0; i < MAX_ELEMENTS; i++ )
+	{
+		list[i] = rand()%1000;
+	}
+	printf("The list before sorting is:\n");
+	printlist_int(list,MAX_ELEMENTS);
+
+	// sort the list using insertion sort
+	shell_sort<int>(list,MAX_ELEMENTS);  
+
+	// print the result
+	printf("The list after sorting using insertion sort algorithm:\n");
+	printlist_int(list,MAX_ELEMENTS);
+	return 0;
+}
